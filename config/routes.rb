91d6f776e32fb4ee27fb2074fc91b2bff195ef03
Andre Carrera <acarrera94@gmail.com>
2017-02-27 11:21:14 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources is a reserved keyword that usees the keywords in the ApplicationController
+  #such as show, new, edit, etc. to route them to the right places.
+end
