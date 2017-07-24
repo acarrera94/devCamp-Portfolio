87da659c615f6fba8cdc8b563c4a6a873156b4b5
Andre Carrera <acarrera94@gmail.com>
2017-07-24 17:54:30 -0600

    integreated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,24 @@
 class PortfoliosController < ApplicationController
-  def index
-    @portfolio_items =  Portfolio.all
+  def index #this is the action/method to index all portfolio_items
+    @portfolio_items = Portfolio.all #.all calls from the model
+    #@port... makes it available to the view
+  end
+
+  def new #this is the action to create a new item
+      @portfolio_item = Portfolio.new #calling from the model
+      #makes it available to the view
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))# here we tell the form what it's allowed to access
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
 
