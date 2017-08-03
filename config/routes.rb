8939e1b142106e4b98f2ce37b605d11488ac0a80
Andre Carrera <acarrera94@gmail.com>
2017-08-03 15:49:25 -0600

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
 
+  #create my own show action
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  #this is a custom route
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
