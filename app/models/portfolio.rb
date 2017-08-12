2c0e1ab0eb76921c1488c0b98b6acdff2d4e1554
Andre Carrera <acarrera94@gmail.com>
2017-08-11 18:48:43 -0600

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,14 @@ class Portfolio < ApplicationRecord
   def self.angular#this is the best practice to call certail parts of the model AKA scope
     where(subtitle: 'Angular')
   end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_default #this happens after new
+
+  def set_default
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
+
 end
