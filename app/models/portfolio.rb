5cc1b9c803ab0b06f0bb565517c78e8d67f2f1a2
Andre Carrera <acarrera94@gmail.com>
2017-08-16 12:14:31 -0600

    integreated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,9 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank?}
+  #that is data validation to make sure it's not empty
+
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -11,9 +15,9 @@ class Portfolio < ApplicationRecord
 
   after_initialize :set_default #this happens after the new method
 
-  def set_default
+  def set_default #this can be done in the schema file or here to set defaults
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '250')
   end
-
+# ||= means if == to nil then do this.
 end
